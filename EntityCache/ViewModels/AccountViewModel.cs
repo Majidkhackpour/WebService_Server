833909cb541b598f1db534cb207b469431d737da
Ehsan <Ehsan@DESKTOP-N1LU7UQ>
2021-08-24 02:16:17 +0430

    Login View Created

---
+++ EntityCache/ViewModels/AccountViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityCache.ViewModels
+{
+    public class loginViewModel
+    {
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress(ErrorMessage = "آدرس ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "مرا به خاطر بسپار")]
+        public bool RememberMe { get; set; }
+    }
+    public class ForgotPasswordViewModel
+    {
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را پر کنید")]
+        [EmailAddress(ErrorMessage = "آدرس ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+    }
+
+    public class RecoveryPasswordViewModel
+    {
+        [Display(Name = "کلمه عبور جدید")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "تکرار کلمه عبور جدید")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "تکرار کلمه عبور را با دقت وارد کنید")]
+        public string RePassword { get; set; }
+    }
+    public class ChangePassword1ViewMode
+    {
+        [Display(Name = "کلمه عبور فعلی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+
+        [Display(Name = "کلمه عبور جدید")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "تکرار کلمه عبور جدید")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "تکرار کلمه عبور را با دقت وارد کنید")]
+        public string RePassword { get; set; }
+    }
+}
--- Server/Controllers/HomeController.cs
+++ Server/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using EntityCache.ViewModels;
+using Persistence.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Server.Controllers
 {
     public class HomeController : Controller
     {
+        ModelContext db = new ModelContext();
         // GET: Home
         public ActionResult Index()
         {
@@ -29,9 +33,47 @@ namespace Server.Controllers
             return View();
         }
 
+        [Route("Login")]
+        [HttpPost]
+        public ActionResult Login(FormCollection form, loginViewModel Login, string ReturnUrl = "/")
+        {
+            if (ModelState.IsValid)
+            {
+                string hashPassword = FormsAuthentication.HashPasswordForStoringInConfigFile(Login.Password, "MD5");
+                var user = db.Users.SingleOrDefault(u => u.Email == Login.Email && u.Password == hashPassword);
+                if (user != null)
+                {
+                    if (user.IsBlock == false)
+                    {
+                        FormsAuthentication.SetAuthCookie(user.UserName, Login.RememberMe);
+                        return Redirect(ReturnUrl);
+                    }
+                    else 
+                    {
+                        ModelState.AddModelError("Email", "کاربر گرامی حساب کاربری شما توسط سیستم مسدود شده است");
+                        //ViewBag.IsBlock = true;
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("Email", "کاربری با اطلاعات وارد شده یافت نشد");
+                }
+            }
+
+            return View(Login); ;
+        }
+
+        public ActionResult logout()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/");
+        }
+
         public ActionResult Menu()
         {
             return PartialView();
         }
+
+
     }
 }
