ae0d499964c845684cb224124e61d0a4d5781255
arad_enj <arad_enj@yahoo.com>
2022-03-02 22:00:16 +0330

    Commit

---
+++ EntityCache/ViewModels/ActivatioCodeViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using EntityCache.Bussines;
+using Services;
+
+namespace EntityCache.ViewModels
+{
+    public class ActivatioCodeViewModel
+    {
+        public Guid CustomerGuid { get; set; }
+        public string AppSerial { get; set; }
+        public string HardSerial { get; set; }
+        public string CustomerName { get; set; }
+        public string ExpireDateSh { get; set; }
+        public int Term { get; set; } = 12;
+        public string Description { get; set; } = "";
+        public string ActivationCode { get; set; } = "";
+
+        public static async Task<ActivatioCodeViewModel> GenerateAsync(Guid cusGuid)
+        {
+            var item = new ActivatioCodeViewModel();
+            try
+            {
+                var customer = await CustomerBussines.GetAsync(cusGuid);
+                if (customer == null || customer.Guid == Guid.Empty) return item;
+                item.CustomerGuid = customer.Guid;
+                item.CustomerName = customer.Name;
+                item.AppSerial = customer.AppSerial;
+                item.HardSerial = customer.HardSerial;
+                item.ExpireDateSh = customer.ExpireDateSh;
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return item;
+        }
+    }
+}
--- Server/Areas/Manager/Controllers/CustomersController.cs
+++ Server/Areas/Manager/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using EntityCache.ViewModels;
+using Services;
 
 namespace Server.Areas.Manager.Controllers
 {
@@ -12,139 +13,203 @@ namespace Server.Areas.Manager.Controllers
     {
         public async Task<ActionResult> Index()
         {
-            var list = await CustomerBussines.GetAllAsync();
-            return View(list);
+            try
+            {
+                var list = await CustomerBussines.GetAllAsync();
+                return View(list);
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return View();
         }
         public async Task<ActionResult> Details(Guid? id)
         {
-            if (id == null)
+            try
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                if (id == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+                var customers = await CustomerBussines.GetAsync(id.Value);
+                if (customers == null || customers.Guid == Guid.Empty)
+                    return HttpNotFound();
+
+                return PartialView(customers);
             }
-
-            var customers = await CustomerBussines.GetAsync(id.Value);
-            if (customers == null || customers.Guid == Guid.Empty)
-                return HttpNotFound();
-
-            return PartialView(customers);
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return PartialView();
         }
         public async Task<ActionResult> Create()
         {
-            var cus = new CustomerBussines();
-            var allProduct = await ProductBussines.GetAllAsync();
-            foreach (var prd in allProduct)
+            try
             {
-                cus.CustomerSerial.Add(new CustomerSerialViewModel()
+                var cus = new CustomerBussines();
+                var allProduct = await ProductBussines.GetAllAsync();
+                foreach (var prd in allProduct)
                 {
-                    IsChecked = false,
-                    ProductCode = prd.Code,
-                    ProductGuid = prd.Guid,
-                    ProductName = prd.Name
-                });
+                    cus.CustomerSerial.Add(new CustomerSerialViewModel()
+                    {
+                        IsChecked = false,
+                        ProductCode = prd.Code,
+                        ProductGuid = prd.Guid,
+                        ProductName = prd.Name
+                    });
+                }
+                return View(cus);
             }
-            return View(cus);
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "Name,CompanyName,NationalCode,PostalCode,AppSerial,Address,Tell1,Tell2,Tell3,Tell4,Email,HardSerial,SiteUrl,Address,Description,CustomerSerial")] CustomerBussines customers)
         {
-            if (!ModelState.IsValid) return View(customers);
-            customers.Guid = Guid.NewGuid();
-            customers.Modified = DateTime.Now;
-            customers.Status = true;
-            customers.Account = 0;
-            customers.CreateDate = DateTime.Now;
-            customers.ExpireDate = DateTime.Now.AddYears(1);
-            customers.isBlock = false;
-            customers.isWebServiceBlock = false;
-            await customers.SaveAsync();
+            try
+            {
+                if (!ModelState.IsValid) return View(customers);
+                customers.Guid = Guid.NewGuid();
+                customers.Modified = DateTime.Now;
+                customers.Status = true;
+                customers.Account = 0;
+                customers.CreateDate = DateTime.Now;
+                customers.ExpireDate = DateTime.Now.AddYears(1);
+                customers.isBlock = false;
+                customers.isWebServiceBlock = false;
+                await customers.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
             return RedirectToAction("Index");
         }
 
         public async Task<ActionResult> Edit(Guid? id)
         {
-            if (id == null)
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var customers = await CustomerBussines.GetAsync(id.Value, true);
-            if (customers == null || customers.Guid == Guid.Empty)
-                return HttpNotFound();
-            return View(customers);
+            try
+            {
+                if (id == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                var customers = await CustomerBussines.GetAsync(id.Value, true);
+                if (customers == null || customers.Guid == Guid.Empty)
+                    return HttpNotFound();
+                return View(customers);
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return View();
         }
         public async Task<ActionResult> AppSerial(Guid? id)
         {
-            if (id == null)
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var customers = await CustomerBussines.GetAsync(id.Value);
-            if (customers == null || customers.Guid == Guid.Empty)
-                return HttpNotFound();
-            return PartialView(customers);
+            try
+            {
+                if (id == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                var customers = await CustomerBussines.GetAsync(id.Value);
+                if (customers == null || customers.Guid == Guid.Empty)
+                    return HttpNotFound();
+                return PartialView(customers);
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return PartialView();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "Guid,Modified,Status,Name,CompanyName,NationalCode,AppSerial,Address,PostalCode,Tell1,Tell2,Tell3,Tell4,Email,Description,ExpireDate,UserGuid,Account,UserName,Password,SiteUrl,HardSerial,LkSerial,isBlock,isWebServiceBlock,CustomerSerial")] CustomerBussines customers, HttpPostAttribute item)
         {
-            if (!ModelState.IsValid) return View(customers);
-            await customers.SaveAsync();
+            try
+            {
+                if (!ModelState.IsValid) return View(customers);
+                await customers.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
             return RedirectToAction("Index");
         }
 
         public async Task<ActionResult> Delete(Guid? id)
         {
-            if (id == null)
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var customers = await CustomerBussines.GetAsync(id.Value);
-            if (customers == null || customers.Guid == Guid.Empty)
-                return HttpNotFound();
-            return PartialView(customers);
+            try
+            {
+                if (id == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                var customers = await CustomerBussines.GetAsync(id.Value);
+                if (customers == null || customers.Guid == Guid.Empty)
+                    return HttpNotFound();
+                return PartialView(customers);
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return PartialView();
         }
 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(Guid id)
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
         {
-            //Customers customers = db.Customers.Find(id);
-            //db.Customers.Remove(customers);
-            //db.SaveChanges();
+            try
+            {
+                var customers = await CustomerBussines.GetAsync(id);
+                customers.Status = false;
+                await customers.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
             return RedirectToAction("Index");
         }
 
         public async Task<ActionResult> ActivationCode(Guid? id)
         {
-            if (id == null)
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var customers = await CustomerBussines.GetAsync(id.Value);
-            if (customers == null || customers.Guid == Guid.Empty)
-                return HttpNotFound();
-            return View(customers);
+            try
+            {
+                if (id == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                var item = await ActivatioCodeViewModel.GenerateAsync(id.Value);
+                return View(item);
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> ActivationCode(Guid id)
+        public async Task<ActionResult> ActivationCode([Bind(Include = "CustomerGuid,AppSerial,HardSerial,CustomerName,ExpireDateSh,Term,Description,ActivationCode")] ActivatioCodeViewModel activationModel)
         {
-            var customers = await CustomerBussines.GetAsync(id);
+            var a = activationModel;
             if (ModelState.IsValid)
             {
-                var a = customers.Guid;
-                var b = customers.UserGuid;
-                var c = customers.Name;
-                var d = customers.ExpireDateSh;
-                var e = customers.AppSerial;
-                var f = customers.UserName;
+                
                 return RedirectToAction("Index");
             }
-            return View(customers);
-        }
-        public async Task<ActionResult> GeneratActivationCode(Guid id, int month)
-        {
-            var customer = await CustomerBussines.GetAsync(id);
-            if (ModelState.IsValid && customer != null && customer.Guid != Guid.Empty)
-            {
-                var activecode = GenerateActivationCodeServer.ActivationCode(month, customer.HardSerial);
-            }
             return View();
+        }
+        public async Task<ActionResult> GeneratActivationCode(ActivatioCodeViewModel id)
+        {
+            id.ActivationCode = GenerateActivationCodeServer.ActivationCode(id.Term, id.HardSerial);
+            return PartialView(id);
         }
         public async Task<ActionResult> ActivationSmsCode(Guid? id)
         {
@@ -173,41 +238,71 @@ namespace Server.Areas.Manager.Controllers
 
         public async Task<ActionResult> isBlock(Guid? id)
         {
-            if (id == null)
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var customers = await CustomerBussines.GetAsync(id.Value);
-            if (customers == null || customers.Guid == Guid.Empty)
-                return HttpNotFound();
-            return PartialView(customers);
+            try
+            {
+                if (id == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                var customers = await CustomerBussines.GetAsync(id.Value);
+                if (customers == null || customers.Guid == Guid.Empty)
+                    return HttpNotFound();
+                return PartialView(customers);
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return PartialView();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> isBlock(Guid id)
         {
-            var customers = await CustomerBussines.GetAsync(id);
-            customers.isBlock = !customers.isBlock;
-            await customers.SaveAsync();
+            try
+            {
+                var customers = await CustomerBussines.GetAsync(id);
+                customers.isBlock = !customers.isBlock;
+                await customers.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
             return RedirectToAction("Index");
         }
 
         public async Task<ActionResult> isWebServiceBlock(Guid? id)
         {
-            if (id == null)
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            var customers = await CustomerBussines.GetAsync(id.Value);
-            if (customers == null || customers.Guid == Guid.Empty)
-                return HttpNotFound();
-            return PartialView(customers);
+            try
+            {
+                if (id == null)
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                var customers = await CustomerBussines.GetAsync(id.Value);
+                if (customers == null || customers.Guid == Guid.Empty)
+                    return HttpNotFound();
+                return PartialView(customers);
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return PartialView();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> isWebServiceBlock(Guid id)
         {
-            var customers = await CustomerBussines.GetAsync(id);
-            customers.isWebServiceBlock = !customers.isWebServiceBlock;
-            await customers.SaveAsync();
+            try
+            {
+                var customers = await CustomerBussines.GetAsync(id);
+                customers.isWebServiceBlock = !customers.isWebServiceBlock;
+                await customers.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
             return RedirectToAction("Index");
         }
     }
